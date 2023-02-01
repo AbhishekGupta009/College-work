c87dacba3717cfeca4278fb0b8b688a4e75f3dca
AbhishekGupta009 <tech88abhi@gmail.com>
2023-02-01 15:04:52 +0530

    new

--- labwork.py
+++ labwork.py
@@ -1,49 +1,49 @@
 import pandas as pd
 
-data = {
+collection = {
     "Employee ID": ["161E90", "161F91", "161F99", "171E20", "171G30"],
     "Name": ["Raman", "Himadri", "Jaya", "Tejas", "Ajay"],
     "Age": [41, 38, 51, 30, 45],
     "Salary(PM)": [56000, 67500, 82100, 55000, 44000]
 }
 
-df = pd.DataFrame(data)
+df = pd.DataFrame(collection)
 
 
 class Employee:
-    def __init_(self, data):
+    def __init__(self, data):
         self.data = data
 
-    def sort_age(self):
+    def age(self):
         self.sdf = self.data.sort_values(by=["Age"], ascending=True)
         return self.sdf
 
-    def sort_name(self):
+    def name(self):
         self.sdf = self.data.sort_values(by=["Name"], ascending=True)
         return self.sdf
 
-    def sort_salary(self):
+    def salary(self):
         self.sdf = self.data.sort_values(by=["Salary(PM)"], ascending=True)
         return self.sdf
 
 
 ob = Employee(df)
 
-choice = 1
+choose = 1
 
-while choice:
+while choose:
     print("0. Exit")
     print("1. Age")
     print("2. Name")
     print("3. Salary")
-    choice = int(input("Enter your choice:"))
-    if choice == 1:
+    choose = int(input("Enter your choice:"))
+    if choose == 1:
         print(ob.sort_age())
-    elif choice == 2:
+    elif choose == 2:
         print(ob.sort_name())
-    elif choice == 3:
+    elif choose == 3:
         print(ob.sort_salary())
-    elif choice == 0:
+    elif choose == 0:
         print("Exiting")
     else:
-        print("Invalid Option")
+        print("Wrong ans")
